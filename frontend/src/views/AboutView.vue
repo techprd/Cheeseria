6865e19fca5703c0e4842bc3ad7093b0e277b8af
Ali shirzad <shirzad@techprd.com>
2022-06-12 21:49:53 +1000

    updated github link in about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -3,8 +3,8 @@
     <h1>Cheeseria App</h1>
     <h2>FullStack App built on Springboot and Vue JS</h2>
     <p>
-      Github: <a href="https://github.com/techprd/invoiceme" target="_blank">
-      <img src="https://img.shields.io/github/forks/techprd/kotlin_node_js_seed.svg?style=social" alt="Cheeseria">
+      Github: <a href="https://github.com/techprd/Cheeseria" target="_blank">
+      <img src="https://img.shields.io/github/forks/techprd/Cheeseria.svg?style=social" alt="Cheeseria">
     </a>
     </p>
   </div>
